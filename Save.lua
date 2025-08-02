0d7bf0a7a3b029715cf88f3b94198312acdbaa7a
xshiba <92438125+xshiba@users.noreply.github.com>
2025-08-03 03:23:12 +0700

    Update Save.lua

--- Save.lua
+++ Save.lua
@@ -119,6 +119,7 @@ local SaveManager = {} do
 		for _, option in next, decoded.objects do
 			if self.Parser[option.type] then
 				self.Parser[option.type].Load(option.idx, option) -- task.spawn() so the config loading wont get stuck.
+				wait(.1)
 			end
 		end
 
